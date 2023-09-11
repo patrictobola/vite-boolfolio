033b334dda9585064059291c27a4c048b4c66343
patrictobola <tobola92@gmail.com>
2023-09-11 17:02:32 +0200

    compiled cards

--- src/components/projects/ProjectsCard.vue
+++ src/components/projects/ProjectsCard.vue
@@ -8,11 +8,9 @@ export default {
         <div class="card">
             <div class="card-body">
                 <h5 class="card-title">{{ project.title }}</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{ project.date }}</h6>
+                <p class="card-text">{{ project.description }}</p>
+
             </div>
         </div>
     </li>
