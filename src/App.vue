6ce1989c799be0286c07c42dd2842b50c6d2b93a
patrictobola <tobola92@gmail.com>
2023-09-11 16:23:21 +0200

    tested in console if axios get the call in db

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
 export default {
   components: {
-    AppHeader
+    AppHeader, AppMain
+  },
+  methods: {
+    fetchProjects() {
+      const endpoint = 'http://127.0.0.1:8000/api/projects';
+      axios.get(endpoint).then(res => { console.log(res.data) })
+    }
+  },
+  created() {
+    this.fetchProjects();
   }
 }
 </script>
 
 <template>
   <AppHeader />
+  <AppMain />
 </template>
 
 <style ></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,7 @@
+<script></script>
+<template>
+    <main class="container mt-5">
+        <h1>Projects</h1>
+    </main>
+</template>
+<style></style>
